1ae01a7525c75c9e9a7c1998e9ad0dbd8a512bc1
sunithasomasundaran <127031460+sunithasomasundaran@users.noreply.github.com>
2023-03-05 14:30:45 +0000

    pep8 formating

--- lineplot.py
+++ lineplot.py
@@ -50,29 +50,30 @@ def getticksforyears(start_year, end_year, step):
     years = np.arange(start_year, end_year + 1, step)
     return years
 
-#reading the dataset from an excel file.
+
+# reading the dataset from an excel file.
 electricity_df = pd.read_excel('electricity.xlsx')
 
-#extract the data we neeed for plotting from the dataframe
-#using indexing and slicing.
+# extract the data we neeed for plotting from the dataframe
+# using indexing and slicing.
 years = electricity_df.columns.values[2:]
 
-#The list of countries being compared in the plot
+# The list of countries being compared in the plot
 countries = ['India', 'Pakistan', 'Sri Lanka', 'Bangladesh']
 
-#Set the font size for all the texts appearing on the plot
+# Set the font size for all the texts appearing on the plot
 plt.rcParams.update({'font.size': 22})
 
-#Initializing a figure for plotting the chart
+# Initializing a figure for plotting the chart
 plt.figure(figsize=[15, 10], dpi=600)
 
-#plotting multiple line charts for the list of countries
+# plotting multiple line charts for the list of countries
 for c in countries:
     plt.plot(years, getpopulationbycountry(c), linewidth=4)
 
-#improving the readability of the chart by providing a meaningful title,
-#informative legend
-#and proper y-axis and x-axis tick values.
+# improving the readability of the chart by providing a meaningful title,
+# informative legend
+# and proper y-axis and x-axis tick values.
 plt.title('''\nRural population with access to electricity - 2000 to 2020\n''',
           fontsize='x-large',
           fontweight='bold')
@@ -80,14 +81,13 @@ plt.legend(countries, loc=4)
 plt.yticks(np.arange(0, 110, 10))
 plt.xticks(getticksforyears(years.min(), years.max(), 1), rotation=60)
 
-#labelling the x-axis and y-axis
+# labelling the x-axis and y-axis
 plt.xlabel('Years')
 plt.ylabel('population (%)')
 
-#automatically adjust the layout of the plot
 plt.tight_layout()
 
-#saving the plot as an image
+# saving the plot as an image
 plt.savefig("lineplot.png")
 
 plt.show()
