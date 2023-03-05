f8aeec2a012e8901bd3801abb6753d113ae13354
sunithasomasundaran <127031460+sunithasomasundaran@users.noreply.github.com>
2023-03-05 14:30:08 +0000

    pep-8 formatting

--- barchart.py
+++ barchart.py
@@ -46,31 +46,32 @@ def geturbanpopulation(year):
         'Access to electricity, urban (% of urban population)', year]
     return urban_population
 
-#reading the dataset from an excel file.
+
+# reading the dataset from an excel file.
 electricity_df = pd.read_excel('electricity.xlsx')
 
-#extracting the data we neeed for plotting from the dataframe
-#using indexing.
+# extracting the data we neeed for plotting from the dataframe
+# using indexing.
 electricity_df.set_index('Series Name', inplace=True)
 countries = electricity_df['Country Name'].unique()
 
-#Set the font size for all the texts appearing on the plot
+# Set the font size for all the texts appearing on the plot
 plt.rcParams.update({'font.size': 22})
 
-#Initializing a figure for plotting the chart
+# Initializing a figure for plotting the chart
 plt.figure(figsize=[15, 10], dpi=600)
 
-#setting the offset value to arrange the bars
+# setting the offset value to arrange the bars
 bar_position = np.arange(len(countries))
 
-#plotting multiple bar charts for the urban and rural population
-#for the list of countries.
+# plotting multiple bar charts for the urban and rural population
+# for the list of countries.
 plt.bar(bar_position, getruralpopulation(2005), width=0.25)
 plt.bar(bar_position + 0.25, geturbanpopulation(2005), width=0.25)
 
-#improving the readability of the chart by providing a meaningful title,
-#informative legend
-#and proper y-axis and x-axis tick values.
+# improving the readability of the chart by providing a meaningful title,
+# informative legend
+# and proper y-axis and x-axis tick values.
 plt.title('''\nPopulation with access to electricity - 2005\n''',
           fontsize='x-large',
           fontweight='bold')
@@ -78,14 +79,13 @@ plt.legend(['Rural Population', 'Urban Population'])
 plt.xticks([c + 0.25 for c in range(len(countries))], countries, rotation=60)
 plt.yticks(np.arange(0, 160, 20))
 
-#labelling the x-axis and y-axis
+# labelling the x-axis and y-axis
 plt.xlabel('Countries')
 plt.ylabel('population (%)')
 
-#adjust the layout of the figure
 plt.tight_layout()
 
-#saving the plot as an image
+# saving the plot as an image
 plt.savefig("barplot.png")
 
 plt.show()
